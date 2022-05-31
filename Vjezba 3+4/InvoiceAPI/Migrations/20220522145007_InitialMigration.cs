0569d30684f4704b7bfffe731a4dd623905ae500
ile <74355346+ile123@users.noreply.github.com>
2022-05-31 11:40:29 +0200

    Add files via upload

---
+++ Vjezba 3+4/InvoiceAPI/Migrations/20220522145007_InitialMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvoiceAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PayerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PayerAdress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PayerIBAN = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PayerNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RecieverName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RecieverAdress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RecieverNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Emergency = table.Column<bool>(type: "bit", nullable: false),
+                    Amount = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invoices");
+        }
+    }
+}
